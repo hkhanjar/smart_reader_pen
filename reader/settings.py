822da4c6925fbe231ccc39efd75b0a41a68cae77
Houria khanjar <37150931+hkhanjar@users.noreply.github.com>
2020-01-11 20:26:53 +0200

    edit settings

--- reader/settings.py
+++ reader/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
